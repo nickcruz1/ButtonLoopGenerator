e67d2db8128d285edf89ab85084118e990a1856c
Nick Cruz <nickcruz920@gmail.com>
2023-01-05 08:31:54 -0500

    Added start btn loop generator function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function startBtnLoop() {
 
+let btn1 = `<button class="btn btn-primary">Button 1</button>`;
+let btn2 = `<button class="btn btn-warning">Button 2</button>`;
+let btn3 = `<button class="btn btn-danger">Button 3</button>`;
+
+let group = [btn1, btn2, btn3];
+
+for(let i = 0; i < group.length; i++) {
+  document.getElementById("output").innerHTML = group;
+}
+  
+}
