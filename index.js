d24e87d131993b2e0b0571d11819110cadcf84ea
Nick Cruz <nickcruz920@gmail.com>
2023-01-06 09:10:11 -0500

    Reorder structure of all js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function startBtnLoop() {
 
+let insertText = document.querySelector(".insertText").value;
 let btn1 = `<button class="btn btn-primary">Button 1</button>`;
 let btn2 = `<button class="btn btn-warning">Button 2</button>`;
 let btn3 = `<button class="btn btn-danger">Button 3</button>`;
 
 let group = [btn1, btn2, btn3];
-
-for(let i = 0; i < group.length; i++) {
-  document.getElementById("output").innerHTML = group;
-}
   
-}
+  if(insertText == "Nick") {
+    for(let i = 0; i < group.length; i++) {
+  document.getElementById("output").innerHTML = group;
+} 
+   } else {
+    return false;
+  } 
 
+}
 
 function refresh() {
   let btn1 = `<button class="btn btn-primary">Button 1</button>`;
